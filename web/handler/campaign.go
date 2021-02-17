c2e91ecc53e960dc6fb30f9ae7e726b45d28c26c
Nico <nicosiput@gmail.com>
2021-02-17 14:49:50 +0700

    WIP: upload campaign image

--- web/handler/campaign.go
+++ web/handler/campaign.go
@@ -3,7 +3,9 @@ package handler
 import (
 	"bwastartup/campaign"
 	"bwastartup/user"
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -75,6 +77,64 @@ func (h *campaignHandler) Create(c *gin.Context) {
 	_, err = h.campaignService.CreateCampaign(createCampaignInput)
 	if err != nil {
 		c.HTML(http.StatusInternalServerError, "error.html", nil)
+		return
+	}
+
+	c.Redirect(http.StatusFound, "/campaigns")
+
+}
+
+func (h *campaignHandler) NewImage(c *gin.Context) {
+	idParam := c.Param("id")
+	id, _ := strconv.Atoi(idParam)
+
+	c.HTML(http.StatusOK, "campaign_image.html", gin.H{"ID": id})
+}
+
+func (h *campaignHandler) CreateImage(c *gin.Context) {
+	file, err := c.FormFile("file")
+	if err != nil {
+		c.HTML(http.StatusInternalServerError, "error.html", nil)
+		return
+	}
+
+	idParam := c.Param("id")
+	id, _ := strconv.Atoi(idParam)
+
+	existingCampaign, err := h.campaignService.GetCampaignById(campaign.GetCampaignDetailInput{ID: id})
+	if err != nil {
+		c.HTML(http.StatusInternalServerError, "error.html", nil)
+		fmt.Println(err)
+		return
+	}
+
+	userID := existingCampaign.UserID
+	path := fmt.Sprintf("images/%d-%s", userID, file.Filename)
+
+	err = c.SaveUploadedFile(file, path)
+	if err != nil {
+		c.HTML(http.StatusInternalServerError, "error.html", nil)
+		fmt.Println(err)
+		return
+	}
+
+	createCampaignImageInput := campaign.CreateCampaignImageInput{}
+	createCampaignImageInput.CampaignID = id
+	createCampaignImageInput.IsPrimary = true
+
+	userCampaign, err := h.userService.GetUserById(userID)
+	if err != nil {
+		c.HTML(http.StatusInternalServerError, "error.html", nil)
+		fmt.Println(err)
+		return
+	}
+
+	createCampaignImageInput.User = userCampaign
+
+	_, err = h.campaignService.SaveCampaignImage(createCampaignImageInput, path)
+	if err != nil {
+		c.HTML(http.StatusInternalServerError, "error.html", nil)
+		fmt.Println(err)
 		return
 	}
 
